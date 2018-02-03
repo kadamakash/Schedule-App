8a0356ed1d64b7d5b389954d8aa3f625c1854cc8
akashmk <akash.kadam@healable.com>
2018-02-02 19:51:39 -0500

    Inception

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyTeamsPage } from '../pages/my-teams/my-teams.page';
+import { TournamentsPage } from '../pages/tournaments/tournaments';
+import { TeamDetailPage } from '../pages/team-detail/team-detail';
+import { GamePage } from '../pages/game/game';
+import { TeamsPage } from '../pages/pages';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    MyTeamsPage,
+    TournamentsPage,
+    TeamDetailPage,
+    GamePage,
+    TeamsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    MyTeamsPage,
+    TournamentsPage,
+    TeamDetailPage,
+    GamePage,
+    TeamsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/my-teams/my-teams.page.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core/src/metadata/directives";
+import { NavController } from 'ionic-angular';
+
+import { TournamentsPage } from '../pages';
+
+@Component({
+    templateUrl: 'build/pages/my-teams/my-teams.page.html'
+})
+
+export class MyTeamsPage{
+
+    constructor(private nav: NavController){}
+
+    goToTournaments(){
+        this.nav.push(TournamentsPage);
+    }
+}
